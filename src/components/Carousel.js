e8f0350bfa9796aa7d616c716cc8d3272482f365
jitunmohajan <jitunmohajan@gmail.com>
2023-07-29 22:34:14 +0600

    added navbar and carousel

---
+++ src/components/Carousel.js
@@ -0,0 +1,64 @@
+import Image from 'next/image';
+import React from 'react';
+import BannerImage from '@/assets/images/banner-1.jpg'
+
+const Carousel = () => {
+    return (
+        <div className="carousel w-full">
+            <div id="slide1" className="carousel-item relative w-full">
+                <Image
+                    src={BannerImage}
+                    alt="Example Image"
+                    width={300}
+                    height={200}
+                    layout="responsive"
+                />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <a href="#slide4" className="btn btn-circle">❮</a> 
+                <a href="#slide2" className="btn btn-circle">❯</a>
+                </div>
+            </div> 
+            <div id="slide2" className="carousel-item relative w-full">
+                <Image
+                    src={BannerImage}
+                    alt="Example Image"
+                    width={300}
+                    height={200}
+                    layout="responsive"
+                />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <a href="#slide1" className="btn btn-circle">❮</a> 
+                <a href="#slide3" className="btn btn-circle">❯</a>
+                </div>
+            </div> 
+            <div id="slide3" className="carousel-item relative w-full">
+                <Image
+                    src={BannerImage}
+                    alt="Example Image"
+                    width={300}
+                    height={200}
+                    layout="responsive"
+                />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <a href="#slide2" className="btn btn-circle">❮</a> 
+                <a href="#slide4" className="btn btn-circle">❯</a>
+                </div>
+            </div> 
+            <div id="slide4" className="carousel-item relative w-full">
+                <Image
+                    src={BannerImage}
+                    alt="Example Image"
+                    width={300}
+                    height={200}
+                    layout="responsive"
+                />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <a href="#slide3" className="btn btn-circle">❮</a> 
+                <a href="#slide1" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Carousel;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
+import Carousel from '@/components/Carousel';
+import Nav from '@/components/Nav';
+import NavBar from '@/components/NavBar';
 import React from 'react';
 
 const HomePage = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+     {/* <NavBar></NavBar> */}
+     <Nav></Nav>
+     {/* <Carousel></Carousel> */}
     </div>
   );
 };
